f8ab64f95d9e2e987be53daa97b9d3a91e134d63
naiduyamini-12 <naiduyamini34@gmail.com>
2024-10-25 16:44:44 +0530

    Create If else condition.c

---
+++ If else condition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the  value of number");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("The number is postive ");
+    }
+    else
+    {
+        printf("The number is negative ");
+    }
+}
