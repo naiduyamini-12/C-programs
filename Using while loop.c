30aad38e72c5106c7b84d57c721597c9a0e82505
naiduyamini-12 <naiduyamini34@gmail.com>
2024-12-20 11:56:20 +0530

    Create Using while loop.c

---
+++ Using while loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int  i=0,j=10;
+    while(i<=10)
+        {
+            printf("%d",i);
+            i++;
+        }
+    while(j>=0)
+        {
+            printf("%d",j);
+            j--;
+        }
+    return(0);
+}
